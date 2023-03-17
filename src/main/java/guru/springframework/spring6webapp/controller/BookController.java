e340ffe613c1caa3188f397a37b4531c6457a7cc
Tejas Sinapanahalli Gangadharaiah <tejas.sg753@gmail.com>
2023-03-17 00:08:12 -0500

    Configuring Spring controller

---
+++ src/main/java/guru/springframework/spring6webapp/controller/BookController.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring6webapp.controller;
+
+import guru.springframework.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : tejas
+ * @created : 3/16/23, Thursday
+ **/
+@Controller
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookService.findAll());
+
+        return "books";
+
+    }
+}
